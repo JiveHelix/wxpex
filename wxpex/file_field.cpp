c0e17ff5bca21129a2649f2e906249e2414fe37c
Jive Helix <jivehelix@gmail.com>
2026-01-15 13:03:01 -0700

    Fixed default directory in FileField.

--- wxpex/file_field.cpp
+++ wxpex/file_field.cpp
@@ -1,5 +1,9 @@
 #include <wxpex/file_field.h>
 
+WXSHIM_PUSH_IGNORES
+#include <wx/filename.h>
+WXSHIM_POP_IGNORES
+
 
 namespace wxpex
 {
@@ -9,12 +13,17 @@ std::optional<std::string> ChoosePath(
     const std::string &currentValue,
     const FileDialogOptions &options)
 {
+    auto defaultDir =
+        currentValue.empty()
+        ? wxFileName::GetHomeDir()
+        : wxString(currentValue);
+
     if (options.isFolder)
     {
         wxDirDialog openFolder(
             nullptr,
             wxString(options.message),
-            wxString(currentValue),
+            defaultDir,
             (options.style & wxFD_FILE_MUST_EXIST)
                 ? wxDD_DIR_MUST_EXIST | wxDD_DEFAULT_STYLE
                 : wxDD_DEFAULT_STYLE);
